36538f456588acc79322e8201895cc939dc30489
Ashi-Shukla <74825506+Ashi-Shukla@users.noreply.github.com>
2020-11-30 02:51:52 +0530

    sketch file updated

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,19 +6,16 @@ const Render = Matter.Render;
 var dustbinObj, paperObject,groundObject	
 var world;
 
-
 function setup() {
-	createCanvas(1600, 700);
+	createCanvas(1250, 700);
 	rectMode(CENTER);
-
 
 	engine = Engine.create();
 	world = engine.world;
-	dustbinObj=new dustbin(1200,650);
+	dustbinObj=new dustbin(1050,650);
 	paperObject=new paper(200,450,40);
 	groundObject=new ground(width/2,670,width,20);
 	//Create a Ground
-	
 
 	var render = Render.create({
 	  element: document.body,
@@ -32,10 +28,7 @@ function setup() {
 	});
 
 	Engine.run(engine);
-	//Render.run(render);
-  
 }
-
 
 function draw() {
   rectMode(CENTER);
@@ -43,23 +36,11 @@ function draw() {
  
   dustbinObj.display();
   paperObject.display();
-  groundObject.display();
-  
- 
-  
-  
- 
+  groundObject.display(); 
 }
 
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
-
-    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-    
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:75,y:-75});
   	}
 }
-
-
-
-
-
